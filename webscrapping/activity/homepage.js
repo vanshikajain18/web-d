5242fb4861ccfbe51f28ccb19c6ceabd29f3700c
Vanshika <vanshika.jain2000@gmail.com>
2021-01-12 19:41:25 +0530

    facts

---
+++ webscrapping/activity/homepage.js
@@ -0,0 +1,24 @@
+// request is a module which allows us to make http calls
+//cheerio is used to work on html elements 
+
+const request= require("request") ;
+const cheerio= require("cheerio") ;
+
+request("https://www.espncricinfo.com/series/ipl-2020-21-1210595",cb) ;  //gets the html of the link via cb 
+
+function cb(error,response,data)
+{
+    if(error==null)
+    parseData(data) ;
+
+    else console.log(error) ;
+}
+
+
+function parseData(data)        //data= html of the link
+{
+    const ch= cheerio.load(data) ;
+    let link= ch(".widget-items.cta-link a").attr("href") ;
+     let completeLink= `https://www.espncricinfo.com${link}` ;
+     console.log(completeLink) ;
+}
---
+++ webscrapping/facts (module.exports)/test1.js
@@ -0,0 +1,12 @@
+const { text ,text1: abcd} = require("./test2");           //text1 is given an alias 
+// syntax to import only some keys 
+
+//to import entire object
+// const obj = require("./test2");
+// console.log(obj.text) ;
+
+let text1="hi i am text of file 1" ;
+
+
+console.log(text) ;
+console.log(abcd) ;
