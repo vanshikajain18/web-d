8080429c9b4dbe76e0df7f9ee759254937c2d0ff
Vanshika <vanshika.jain2000@gmail.com>
2021-01-12 19:42:50 +0530

    facts

---
+++ webscrapping/facts (module.exports)/test2.js
@@ -0,0 +1,11 @@
+let text2 = "hi i am text of file 2" ;
+let val="sgfhdj" ;
+
+//node js gives us an object called module.exports
+// module.exports={
+//    text : text2 ;
+//    text1 : val ;
+//  }
+
+module.exports.text= text2 ; // a key "text" is formed 
+module.exports.text1= val ; 
