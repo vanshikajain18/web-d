96826f729724becb90591e89b9236816dcc3bd05
Vanshika <vanshika.jain2000@gmail.com>
2021-02-11 19:42:00 +0530

    addChallenge automation

--- async await/activity/addchallenge.js
+++ async await/activity/addchallenge.js
@@ -31,22 +31,18 @@ const pwd  = "12345678";
            let manageChallengeLi = bothLis[1];
         
            await Promise.all([ tab.waitForNavigation({waitUntil:"networkidle2"}) , manageChallengeLi.click() ]); // navigation
-           await Promise.all([tab.waitForNavigation({waitUntil: "networkidle2"})  , tab.click('.btn.btn-green.backbone.pull-right') ]) ;
+           let addChallengeBtn=  await  tab.$('.btn.btn-green.backbone.pull-right')  ;
            //reached challenge add page 
-           await tab.type("#name", );
-           await tab.type("#preview ", );
-           await tab.type(".CodeMirror-line __web-inspector-hide-shortcut__", );
-           await tab.type(" ", );
-           await tab.type(" ", );
+        
+           let addChallengeLink = await tab.evaluate( function(element){   return element.getAttribute("href");  }  , addChallengeBtn);
+           addChallengeLink = `https://www.hackerrank.com${addChallengeLink}` ;
 
-
-
-
-
-
-
-
-
+           //loop  to add all challenges 
+           for(let i=0 ;i<challenges.length; i++)
+           {
+               let newTab = await browser.newPage() ;
+               addChallenge(newTab, challenges[i], addChallengeLink,i) ;
+           }
     }
 
     catch(error){
@@ -54,3 +50,34 @@ const pwd  = "12345678";
     }
 }) () ;
      
+
+async function addChallenge(newTab, challenge, link ,i)
+{
+    try{
+        let challengeName = challenge["Challenge Name"];
+        let description = challenge["Description"];
+        let problemStatement = challenge["Problem Statement"];
+        let inputFormat = challenge["Input Format"];
+        let constraints = challenge["Constraints"];
+        let outputFormat = challenge["Output Format"];
+        let tags = challenge["Tags"];
+
+        await newTab.goto(link);
+        await newTab.waitForSelector('#name' , {timeout: 10000, visible:true});
+        await newTab.type("#name" , challengeName);
+        await newTab.type("#preview" , description);
+        await newTab.waitForSelector('#problem_statement-container .CodeMirror textarea'  , {timeout: 10000,visible:true});
+        await newTab.type('#problem_statement-container .CodeMirror textarea' , problemStatement);
+        await newTab.type('#input_format-container .CodeMirror textarea' , inputFormat);
+        await newTab.type('#constraints-container .CodeMirror textarea' , constraints);
+        await newTab.type('#output_format-container .CodeMirror textarea' , outputFormat);
+        await newTab.type('#tags_tag' , tags);
+        await newTab.keyboard.press("Enter");
+        await newTab.click(".save-challenge.btn.btn-green") ;
+        console.log("challenge " +i +" added !") ;
+        await newTab.close();;
+    }
+    catch(error){
+        console.log(error);
+    }
+}
